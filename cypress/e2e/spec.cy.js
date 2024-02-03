1ea038f0ec98ff01a24660221ee1149a4eff87e8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-03 18:31:45 -0500

    two functions

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,14 +1,5 @@
-function formatPrice(price) {
-  // return string starting with "$" sign
-  // separate thousands with comma
-  // formatted for USA
-  let formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-  })
-
-  return formatter.format(price)
-}
+function add(a, b) {}
+function formatPrice(price) {}
 
 describe('template spec', () => {
   it('passes', () => {
