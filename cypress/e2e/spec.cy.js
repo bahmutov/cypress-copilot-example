878cbda4e5518424225f58fd3e20ce958453b18e
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-08 11:35:59 -0500

    solution

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('todomvc application', () => {
+  it('loads the app', () => {
     cy.visit('/')
+    // the should be loaded
+    // and have class "loaded" set on the body element
+    cy.get('body').should('have.class', 'loaded')
+    // check the title
+    // it should be "cypress-copilot-example"
+    cy.title().should('equal', 'cypress-copilot-example')
+    // the h1 heading should have text "todos"
+    cy.contains('h1', 'todos')
+      // should have attribute "data-cy" set to "app-title"
+      .should('have.attr', 'data-cy', 'app-title')
   })
 })
