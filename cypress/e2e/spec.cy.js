3ca1e89aba2bbfc3c1531d51847c37fcbe38a148
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-08 11:36:22 -0500

    a5 starting code

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,13 +3,11 @@ describe('todomvc application', () => {
     cy.visit('/')
     // the should be loaded
     // and have class "loaded" set on the body element
-    cy.get('body').should('have.class', 'loaded')
+
     // check the title
     // it should be "cypress-copilot-example"
-    cy.title().should('equal', 'cypress-copilot-example')
+
     // the h1 heading should have text "todos"
-    cy.contains('h1', 'todos')
-      // should have attribute "data-cy" set to "app-title"
-      .should('have.attr', 'data-cy', 'app-title')
+    // should have attribute "data-cy" set to "app-title"
   })
 })
