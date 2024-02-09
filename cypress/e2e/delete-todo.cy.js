9c5741e76a1ff38208642b352a603111154ae9c8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-09 17:26:54 -0500

    initial code for a7

--- cypress/e2e/delete-todo.cy.js
+++ cypress/e2e/delete-todo.cy.js
@@ -1,19 +1,1 @@
 /// <reference types="cypress" />
-
-describe('Delete todo', () => {
-  beforeEach(() => {
-    // load the fixture three-todos.json
-    cy.fixture('three-todos').then((todos) => {
-      cy.request('POST', '/reset', { todos })
-    })
-  })
-
-  it('deletes a todo', () => {
-    cy.visit('/')
-    cy.get('.todo-list li').should('have.length', 3)
-    cy.contains('.todo-list li', 'Complete project report')
-      .find('.destroy')
-      .click({ force: true })
-    cy.get('.todo-list li').should('have.length', 2)
-  })
-})
