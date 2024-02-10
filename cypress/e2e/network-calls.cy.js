6dbf1ba148ff980629875c360d83b815aa156a4f
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-10 12:30:36 -0500

    init the test

--- cypress/e2e/network-calls.cy.js
+++ cypress/e2e/network-calls.cy.js
@@ -14,19 +14,4 @@ it('loads todos', () => {
   cy.wait('@save')
 })
 
-it('deletes a todo', () => {
-  // stub the loading network call with fixture "three-todos.json"
-  cy.intercept('GET', '/todos', {
-    fixture: 'three-todos',
-  }).as('load')
-  cy.visit('/')
-  cy.wait('@load')
-  // intercept the "DELETE /todos/1" call
-  cy.intercept('DELETE', '/todos/1', {}).as('delete')
-  // delete the first todo
-  cy.get('.todo-list li')
-    .first()
-    .find('.destroy')
-    .click({ force: true })
-  cy.wait('@delete')
-})
+it('deletes a todo', () => {})
