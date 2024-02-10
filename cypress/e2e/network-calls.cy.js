01a5d477401ede34175fe11fb5d56c8046c3f111
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-10 12:29:28 -0500

    Add delete todo network test

--- cypress/e2e/network-calls.cy.js
+++ cypress/e2e/network-calls.cy.js
@@ -13,3 +13,20 @@ it('loads todos', () => {
   // confirm the "POST /todos" call is made
   cy.wait('@save')
 })
+
+it('deletes a todo', () => {
+  // stub the loading network call with fixture "three-todos.json"
+  cy.intercept('GET', '/todos', {
+    fixture: 'three-todos',
+  }).as('load')
+  cy.visit('/')
+  cy.wait('@load')
+  // intercept the "DELETE /todos/1" call
+  cy.intercept('DELETE', '/todos/1', {}).as('delete')
+  // delete the first todo
+  cy.get('.todo-list li')
+    .first()
+    .find('.destroy')
+    .click({ force: true })
+  cy.wait('@delete')
+})
