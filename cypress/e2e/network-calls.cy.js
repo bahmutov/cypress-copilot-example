32b46c468dc67c92e7bc5297248e39ef721c8329
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-10 12:02:40 -0500

    code

---
+++ cypress/e2e/network-calls.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+it('loads todos', () => {
+  // spy on the "GET /todos" call
+  // using cy.intercept
+  cy.intercept('GET', '/todos').as('load')
+  cy.visit('/')
+  cy.wait('@load')
+
+  // spy on "POST /todos" call
+  cy.intercept('POST', '/todos').as('save')
+  cy.get('.new-todo').type('learn testing{enter}')
+  // confirm the "POST /todos" call is made
+  cy.wait('@save')
+})
