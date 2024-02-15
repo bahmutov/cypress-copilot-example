e5d2ba24db9f14cdbf89c83d749a2ed0089cebdb
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-02-15 15:33:05 -0500

    b4 solution

--- cypress/e2e/adding-todos.cy.js
+++ cypress/e2e/adding-todos.cy.js
@@ -1,5 +1,7 @@
 /// <reference types="cypress" />
 
+import 'cypress-map'
+
 // before each test reset the data
 beforeEach(() => {
   cy.request('POST', '/reset', { todos: [] })
@@ -16,4 +18,30 @@ it('adds a new todo', () => {
   cy.get('.todo-list li').should('have.length', 1)
   // and it should have text "learn testing"
   cy.contains('.todo-list li', 'learn testing')
+})
+
+function addTodo(text) {
+  cy.get('.new-todo').type(`${text}{enter}`)
+}
+
+it('adds four todos', () => {
+  cy.visit('/')
+  cy.get('body').should('have.class', 'loaded')
+  const todo_selector = '.todo-list li label'
+
+  // add four todos
+  addTodo('learn testing')
+  addTodo('be cool')
+  addTodo('be happy')
+  addTodo('be brave')
+
+  // there should be 4 todo items
+  cy.get(todo_selector)
+    .map('innerText')
+    .should('deep.equal', [
+      'learn testing',
+      'be cool',
+      'be happy',
+      'be brave',
+    ])
 })
