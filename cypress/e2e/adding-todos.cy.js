ea9bc7322d5c03753ff8507d66570f94a2721788
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-15 08:47:30 -0400

    starting code

--- cypress/e2e/adding-todos.cy.js
+++ cypress/e2e/adding-todos.cy.js
@@ -20,16 +20,3 @@ it('adds a new todo', () => {
   // and it should have text "learn testing"
   cy.contains('.todo-list li', 'learn testing')
 })
-
-it('adds four todos', () => {
-  cy.visit('/')
-  cy.get('body').should('have.class', 'loaded')
-  // create four todos
-  cy.get('.new-todo').type('learn testing{enter}')
-  cy.get('.new-todo').type('be cool{enter}')
-  cy.get('.new-todo').type('be happy{enter}')
-  cy.get('.new-todo').type('be kind{enter}')
-  // there should be 4 todo items
-  cy.get('.todo-list li').should('have.length', 4)
-  // validate the text of all todos
-})
