dbfa7d8c5f19a7dd855aa270a2e2456d74a0f054
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-05-16 09:21:14 -0400

    comment

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,8 @@
 describe('todomvc application', () => {
   it('loads the app', () => {
     cy.visit('/')
-    // the should be loaded
-    // and have class "loaded" set on the body element
+    // the app should be loaded
+    // ???
 
     // check the title
     // it should be "cypress-copilot-example"
