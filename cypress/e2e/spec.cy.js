39be98506e803c05ccb74eb12b84a236f2117983
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-04-18 15:15:16 -0400

    prepare spec

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,8 @@
 describe('todomvc application', () => {
-  it('loads the app', () => {
+  it('adds a todo', () => {
     cy.visit('/')
-    // the should be loaded
-    // and have class "loaded" set on the body element
-    cy.get('body').should('have.class', 'loaded')
-    // check the title
-    // it should be "cypress-copilot-example"
-    cy.title().should('equal', 'cypress-copilot-example')
-    // the h1 heading should have text "todos"
-    cy.contains('h1', 'todos')
-      // should have attribute "data-cy" set to "app-title"
-      .should('have.attr', 'data-cy', 'app-title')
+    cy.get('.loaded')
+    cy.get('.enter-new-todo').type('New Todo{enter}')
+    cy.get('.todo-list').should('contain', 'New Todo')
   })
 })
