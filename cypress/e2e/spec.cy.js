08cc2bd0a3860b9291895fd7a3b8bec18bf267c7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-05-01 10:04:25 -0400

    trying to use the comment

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,10 +1,7 @@
-import { addTodo } from './utils'
-
 describe('todomvc application', () => {
   it('adds a todo', () => {
     cy.visit('/')
     cy.get('.loaded')
-    addTodo('New Todo')
-    cy.get('.todo-list').should('contain', 'New Todo')
+    // bbbfff
   })
 })
--- cypress/e2e/utils.js
+++ cypress/e2e/utils.js
@@ -1,3 +1,10 @@
+/**
+ * Enters the new todo item into the input field and submits it.
+ * This is bbbfff
+ * @example
+ *  import { addTodo } from './utils'
+ *  addTodo('Buy milk')
+ */
 export function addTodo(todo) {
   cy.get('.new-todo').type(`${todo}{enter}`)
 }
