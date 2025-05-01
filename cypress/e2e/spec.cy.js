faf31053333b519e40b9c8f38064b1bc1e4d8549
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-05-01 09:58:05 -0400

    starting

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,7 +2,7 @@ describe('todomvc application', () => {
   it('adds a todo', () => {
     cy.visit('/')
     cy.get('.loaded')
-    cy.get('.enter-new-todo').type('New Todo{enter}')
+    cy.get('.new-todo').type('New Todo{enter}')
     cy.get('.todo-list').should('contain', 'New Todo')
   })
 })
