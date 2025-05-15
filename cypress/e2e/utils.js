0067002fb97624ba04937f6d2234cc4dfa313f3b
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-05-15 12:54:44 -0400

    initial code

--- cypress/e2e/utils.js
+++ cypress/e2e/utils.js
@@ -1,10 +1,6 @@
 /**
  * Enters the new todo item into the input field and submits it.
- * This is bbbfff
- * @example
- *  import { addTodo } from './utils'
- *  addTodo('Buy milk')
  */
-export function addTodo(todo) {
+export function bbbfff(todo) {
   cy.get('.new-todo').type(`${todo}{enter}`)
 }
